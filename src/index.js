974111e576f8e4749376f511dd25f807f893f588
Danilo <danilojrc@icloud.com>
2020-12-24 16:23:09 -0300

    Create function to convert excel to json

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require('electron')
+const XLSX = require('xlsx')
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -19,8 +20,30 @@ function createWindow() {
     })
 }
 
-function alert() {
-    alert('Teste')
+
+function convertToJSON() {
+    var selectedFile = evt.target.files[0];
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var data = event.target.result;
+        var workbook = XLSX.read(data, {
+            type: 'binary'
+        });
+        workbook.SheetNames.forEach(function (sheetName) {
+
+            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+            var json_object = JSON.stringify(XL_row_object);
+            document.getElementById("jsonObject").innerHTML = json_object;
+
+        })
+    };
+
+    reader.onerror = function (event) {
+        console.error("File could not be read! Code " + event.target.error.code);
+    };
+
+    reader.readAsBinaryString(selectedFile);
+
 }
 
 app.whenReady().then(createWindow)
