7f574bc50f263183798f9d7c2717d082249d451f
Danilo <danilojrc@icloud.com>
2020-12-23 23:28:49 -0300

    Create html and css components

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ function createWindow() {
     const win = new BrowserWindow({
         width: 634,
         height: 394,
+        resizable: false,
+        transparent: false,
+        frame: true,
+        show: false,
         webPreferences: {
             nodeIntegration: true
         }
     })
-
     win.loadFile('./src/index.html')
+
+    win.once("ready-to-show", () => {
+        win.show()
+    })
+}
+
+function alert() {
+    alert('Teste')
 }
 
 app.whenReady().then(createWindow)
