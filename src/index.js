eeb4be95a6b51fff0e2eb6309da9e62c0c244fb0
Danilo <danilojrc@icloud.com>
2020-12-25 00:26:06 -0300

    Create promise function for each file imported

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require('electron')
 const XLSX = require('xlsx')
+let correctedItems
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -20,32 +21,6 @@ function createWindow() {
     })
 }
 
-
-function convertToJSON() {
-    var selectedFile = evt.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function (event) {
-        var data = event.target.result;
-        var workbook = XLSX.read(data, {
-            type: 'binary'
-        });
-        workbook.SheetNames.forEach(function (sheetName) {
-
-            var XL_row_object = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
-            var json_object = JSON.stringify(XL_row_object);
-            document.getElementById("jsonObject").innerHTML = json_object;
-
-        })
-    };
-
-    reader.onerror = function (event) {
-        console.error("File could not be read! Code " + event.target.error.code);
-    };
-
-    reader.readAsBinaryString(selectedFile);
-
-}
-
 app.whenReady().then(createWindow)
 
 app.on('window-all-closed', () => {
@@ -59,3 +34,77 @@ app.on('activate', () => {
         createWindow()
     }
 })
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// 
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function getItems() {
+    return new Promise((resolve, rejected) => {
+        const selectedFile = document.getElementById('inputRightData').files[0]
+        const progressBar = document.getElementById('progressBar')
+        progressBar.style.width = "30%"
+        if (selectedFile) {
+            let fileReader = new FileReader()
+            fileReader.readAsBinaryString(selectedFile)
+            fileReader.onload = (event) => {
+                let data = event.target.result
+                let workbook = XLSX.read(data, { type: "binary" })
+                workbook.SheetNames.forEach(function (sheet, i) {
+                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+                    resolve(rowObject)
+                })
+            }
+        } else {
+            alert('Não foi possível carregar os dados da planilha mãe!')
+            rejected({
+                name: 'Erro',
+                message: 'Não foi possível carregar os itens.'
+            })
+        }
+    })
+}
+
+function getItemsToAnalyze() {
+    return new Promise((resolve, rejected) => {
+        const selectedFile = document.getElementById('inputWrongData').files[0]
+        if (selectedFile) {
+            let fileReader = new FileReader()
+            fileReader.readAsBinaryString(selectedFile)
+            fileReader.onload = (event) => {
+                let data = event.target.result
+                let workbook = XLSX.read(data, { type: "binary" })
+                workbook.SheetNames.forEach(function (sheet, i) {
+                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+                    resolve(rowObject)
+                })
+            }
+        } else {
+            alert('Não foi possível carregar os dados da planilha a ser analisada!')
+            rejected({
+                name: 'Erro',
+                message: 'Não foi possível carregar os itens.'
+            })
+        }
+    })
+}
+
+function compareData() {
+    getItems().then(function (items) {
+        console.log('Loaded itens count: ' + items.length)
+        getItemsToAnalyze().then(function (itemsToAnalyze) {
+            console.log('Loaded itens to analyze count: ' + itemsToAnalyze.length)
+            itemsToAnalyze.forEach(function (item, index) {
+                if (index < 1) {
+                    const barCode = item.REFERENCIA
+                    console.log(barCode)
+                    const testeItem = items.filter(item => {
+                        return item.REFERENCIA === barCode
+                    })
+                    console.log(testeItem[0])
+                    console.log(Object.keys(testeItem[0]))
+                }
+            })
+        })
+    })
+}
