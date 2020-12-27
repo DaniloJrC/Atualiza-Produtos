56e2eb3988a6dc686744d192ee533ede5983ba5f
Danilo <danilojrc@icloud.com>
2020-12-27 15:02:55 -0300

    Refactor html, fix bug in parameters

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ const XLSX = require('xlsx')
 
 let mainWindow
 let productsChecked = []
-var parameters
-var count = 0
+var parameters = []
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
 // Create main window
@@ -24,9 +23,8 @@ function createWindow() {
         frame: true,
         show: false,
         webPreferences: {
-            contextIsolation: true,
             devTools: true,
-            nodeIntegrationInWorker: true,
+            nodeIntegrationInWorker: false,
             nodeIntegration: true
         }
     })
@@ -51,9 +49,179 @@ app.on('activate', () => {
     }
 })
 
-app.on('browser-window-created', function (e, window) {
+/*app.on('browser-window-created', function (e, window) {
     window.setMenu(null)
-})
+})*/
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Load data from excel tables and compare 
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function loadDataFromExcel() {
+    Promise.all([getProductsFromExcel(), getProductsToFixFromExcel()]).then((values) => {
+        shotProgressBarDiv()
+        const productsFromDatabase = values[0]
+        const productsToFixData = values[1]
+        compareDataExtractedFromExcel(productsFromDatabase, productsToFixData)
+    })
+}
+
+function compareDataExtractedFromExcel(productsFromDatabase, productsToFixData) {
+    productsToFixData.forEach(function (productsFound, index) {
+        let value = (index / productsToFixData.length) * 100
+        updateProgressStatus(value)
+        const productsByBarCode = productsFromDatabase.filter(item => {
+            return item.REFERENCIA === productsFound.REFERENCIA
+        })
+        const productToFix = productsByBarCode[0]
+        const fixedProduct = fixProduct(productToFix)
+        addUpdatedProductToJSON(fixedProduct)
+
+    })
+    downloadAsExcel(productsChecked)
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Find products from excel file
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function getProductsFromExcel() {
+    return new Promise((resolve, rejected) => {
+        const selectedFile = document.getElementById('file').files[0]
+        if (selectedFile) {
+            let fileReader = new FileReader()
+            fileReader.readAsBinaryString(selectedFile)
+            fileReader.onload = (event) => {
+                let data = event.target.result
+                let workbook = XLSX.read(data, { type: "binary" })
+                workbook.SheetNames.forEach(function (sheet, i) {
+                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+                    const headerItems = Object.keys(rowObject[0])
+                    parameters = headerItems
+                    resolve(rowObject)
+                })
+            }
+        } else {
+            alert('Não foi possível carregar os dados da planilha mãe!')
+            rejected({
+                name: 'Erro',
+                message: 'Não foi possível carregar os itens.'
+            })
+        }
+    })
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Find products to fix from excel file
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function getProductsToFixFromExcel() {
+    return new Promise((resolve, rejected) => {
+        const selectedFile = document.getElementById('file2').files[0]
+        if (selectedFile) {
+            let fileReader = new FileReader()
+            fileReader.readAsBinaryString(selectedFile)
+            fileReader.onload = (event) => {
+                let data = event.target.result
+                let workbook = XLSX.read(data, { type: "binary" })
+                workbook.SheetNames.forEach(function (sheet, i) {
+                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
+                    resolve(rowObject)
+                })
+            }
+        } else {
+            alert('Não foi possível carregar os dados da planilha a ser analisada!')
+            rejected({
+                name: 'Erro',
+                message: 'Não foi possível carregar os itens.'
+            })
+        }
+    })
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Update input file name on html
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+function updateInputProductsDataSpan(element) {
+    const inputProductsDataSpan = document.getElementById('inputProductsDataSpan')
+    inputProductsDataSpan.textContent = element.value.split('\\').pop()
+    parameters = []
+}
+
+function updateInputProductsToFixDataSpan(element) {
+    const inputProductsToFixDataSpan = document.getElementById('inputProductsToFixDataSpan')
+    inputProductsToFixDataSpan.textContent = element.value.split('\\').pop()
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Fix product data 
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function fixProduct(productToFix) {
+    const newItem = []
+    parameters.forEach(function (currentParameter, index) {
+        const data = {
+            [currentParameter]: productToFix[currentParameter]
+        }
+        newItem.push(data)
+    })
+    const productFixed = {}
+    for (var i = 0; i < newItem.length; i++) {
+        for (var propriedade in newItem[i]) {
+            productFixed[propriedade] = newItem[i][propriedade]
+        }
+    }
+    return productFixed
+}
+
+function addUpdatedProductToJSON(productFixed) {
+    productsChecked.push(productFixed)
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Download new excel table
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function downloadAsExcel(data) {
+    if (data) {
+        var worksheet = XLSX.utils.json_to_sheet(data)
+        const workbook = {
+            Sheets: {
+                'data': worksheet
+            },
+            SheetNames: ['data']
+        }
+
+        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' })
+        saveAsExcel(excelBuffer, 'myFile')
+    }
+}
+
+function saveAsExcel(buffer, filename) {
+    const data = new Blob([buffer], { type: EXCEL_TYPE })
+    saveAs(data, filename + EXCEL_EXTENSION)
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------
+// Update progress status
+//------------------------------------------------------------------------------------------------------------------------------------------------
+
+function shotProgressBarDiv() {
+    const progressBarDiv = document.getElementById('progressBarDiv')
+    progressBarDiv.style.display = "block"
+}
+
+function updateProgressStatus(value) {
+    var bar = document.getElementById('bar')
+    var barLabel = document.getElementById('barLabel')
+    const newValue = value | 0
+    bar.style.width = newValue + '%'
+    barLabel.textContent = newValue + '%'
+    console.log(newValue)
+}
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
 // Create modal window to save settings
@@ -62,7 +230,7 @@ app.on('browser-window-created', function (e, window) {
 function createSettingsWindow() {
     const selectedFile = document.getElementById('file').files[0]
     if (selectedFile) {
-        createColumnsParameters()
+        fetchParameters()
         var settingsModal = document.getElementById("settingsModal")
         settingsModal.style.display = "block"
 
@@ -82,19 +250,36 @@ function closeModal() {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
-// Compare data and create new excel file
+// Fetch parameters from excel file
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
-function createColumnsParameters() {
+function fetchParameters() {
     const checkBoxMainDiv = document.getElementById('checkBoxDiv')
     checkBoxMainDiv.innerHTML = ''
     createLoaderDiv()
-    getItems().then(function (items) {
+    if (parameters !== undefined && parameters.length > 0) {
+        loadLocalParameters()
+    } else {
+        loadParametersFromExcel()
+    }
+}
+
+function loadLocalParameters() {
+    parameters.forEach(function (parameter, index) {
+        createParameterCheckbox(parameter)
+        if (index == parameters.length - 1) {
+            showParameters()
+        }
+    })
+}
+
+function loadParametersFromExcel() {
+    getProductsFromExcel().then(function (products) {
         removeLoaderDiv()
-        const headerItems = Object.keys(items[0])
+        const headerItems = Object.keys(products[0])
         parameters = headerItems
         headerItems.forEach(function (parameter, index) {
-            createParameter(parameter)
+            createParameterCheckbox(parameter)
             if (index == headerItems.length - 1) {
                 showParameters()
             }
@@ -103,10 +288,10 @@ function createColumnsParameters() {
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------
-// Create parameters to update
+// Create parameters checkbox in html
 //------------------------------------------------------------------------------------------------------------------------------------------------
 
-function createParameter(parameter) {
+function createParameterCheckbox(parameter) {
     const checkBoxMainDiv = document.getElementById('checkBoxDiv')
     const checkBoxDiv = document.createElement('div')
     checkBoxDiv.setAttribute('id', 'divBox')
@@ -150,167 +335,5 @@ function removeLoaderDiv() {
 
 function showParameters() {
     removeLoaderDiv()
-    document.getElementById("checkBoxDiv").style.display = "block"
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
-// Load data from excel tables and compare 
-//------------------------------------------------------------------------------------------------------------------------------------------------
-
-function loadDataFromExcel() {
-    Promise.all([getItems(), getItemsToAnalyze()]).then((values) => {
-        const productsFromDatabase = values[0]
-        const productsToFixData = values[1]
-        compareDataExtractedFromExcel(productsFromDatabase, productsToFixData)
-    })
-}
-
-function compareDataExtractedFromExcel(productsFromDatabase, productsToFixData) {
-    productsToFixData.forEach(function (productsFound, index) {
-        let value = (index / productsToFixData.length) * 100
-        updateProgressStatus(value)
-        const productsByBarCode = productsFromDatabase.filter(item => {
-            return item.REFERENCIA === productsFound.REFERENCIA
-        })
-        const productToFix = productsByBarCode[0]
-        const fixedProduct = fixProduct(productToFix)
-        addUpdatedProductToJSON(fixedProduct)
-
-    })
-    downloadAsExcel(productsChecked)
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
-// Fix product data 
-//------------------------------------------------------------------------------------------------------------------------------------------------
-
-function fixProduct(productToFix) {
-    const newItem = []
-    parameters.forEach(function (currentParameter, index) {
-        const data = {
-            [currentParameter]: productToFix[currentParameter]
-        }
-        newItem.push(data)
-    })
-    const productFixed = {}
-    for (var i = 0; i < newItem.length; i++) {
-        for (var propriedade in newItem[i]) {
-            productFixed[propriedade] = newItem[i][propriedade]
-        }
-    }
-    return productFixed
-}
-
-function addUpdatedProductToJSON(productFixed) {
-    productsChecked.push(productFixed)
-}
-
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
-// Download new excel table
-//------------------------------------------------------------------------------------------------------------------------------------------------
-
-function downloadAsExcel(data) {
-    if (data) {
-        var worksheet = XLSX.utils.json_to_sheet(data)
-        const workbook = {
-            Sheets: {
-                'data': worksheet
-            },
-            SheetNames: ['data']
-        }
-
-        const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' })
-        saveAsExcel(excelBuffer, 'myFile')
-    }
-}
-
-function saveAsExcel(buffer, filename) {
-    const data = new Blob([buffer], { type: EXCEL_TYPE })
-    saveAs(data, filename + EXCEL_EXTENSION)
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
-// Update progress status
-//------------------------------------------------------------------------------------------------------------------------------------------------
-
-function updateProgressStatus(value) {
-    var bar = document.getElementById('bar')
-    var barLabel = document.getElementById('barLabel')
-    const newValue = value | 0
-
-    count = 5
-    console.log(count)
-    if (newValue == count) {
-        count = count + 5;
-        console.log(newValue, count)
-        bar.style.width = count + '%';
-        barLabel.textContent = count + '%';
-    } else {
-
-    }
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------------
-// Read data from excel tables
-//------------------------------------------------------------------------------------------------------------------------------------------------
-
-function getItems() {
-    return new Promise((resolve, rejected) => {
-        const selectedFile = document.getElementById('file').files[0]
-        if (selectedFile) {
-            let fileReader = new FileReader()
-            fileReader.readAsBinaryString(selectedFile)
-            fileReader.onload = (event) => {
-                let data = event.target.result
-                let workbook = XLSX.read(data, { type: "binary" })
-                workbook.SheetNames.forEach(function (sheet, i) {
-                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
-                    const headerItems = Object.keys(rowObject[0])
-                    parameters = headerItems
-                    resolve(rowObject)
-                })
-            }
-        } else {
-            alert('Não foi possível carregar os dados da planilha mãe!')
-            rejected({
-                name: 'Erro',
-                message: 'Não foi possível carregar os itens.'
-            })
-        }
-    })
-}
-
-function getItemsToAnalyze() {
-    return new Promise((resolve, rejected) => {
-        const selectedFile = document.getElementById('file2').files[0]
-        if (selectedFile) {
-            let fileReader = new FileReader()
-            fileReader.readAsBinaryString(selectedFile)
-            fileReader.onload = (event) => {
-                let data = event.target.result
-                let workbook = XLSX.read(data, { type: "binary" })
-                workbook.SheetNames.forEach(function (sheet, i) {
-                    let rowObject = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheet])
-                    resolve(rowObject)
-                })
-            }
-        } else {
-            alert('Não foi possível carregar os dados da planilha a ser analisada!')
-            rejected({
-                name: 'Erro',
-                message: 'Não foi possível carregar os itens.'
-            })
-        }
-    })
-}
-
-function updateInputProductsDataSpan(element) {
-    const inputProductsDataSpan = document.getElementById('inputProductsDataSpan')
-    inputProductsDataSpan.textContent = element.value.split('\\').pop()
-}
-
-function updateInputProductsToFixDataSpan(element) {
-    const inputProductsToFixDataSpan = document.getElementById('inputProductsToFixDataSpan')
-    inputProductsToFixDataSpan.textContent = element.value.split('\\').pop()
+    document.getElementById("checkBoxDiv").style.display = "grid"
 }
